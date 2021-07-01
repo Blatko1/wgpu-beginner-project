95448a43987e92dfacdd5e258eceee38babec28f
Leon <kings.of.funny.gemplays@gmail.com>
2021-07-01 18:38:33 +0200

    Added the main state with base fields and a simple vertex and fragment shader.

--- src/main_state.rs
+++ src/main_state.rs
@@ -1,23 +1,113 @@
-pub struct State{
+use wgpu::BackendBit;
 
+pub struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    sc_desc: wgpu::SwapChainDescriptor,
+    swap_chain: wgpu::SwapChain,
+    size: winit::dpi::PhysicalSize<u32>,
+    render_pipeline: wgpu::RenderPipeline
 }
 
-impl State{
+impl State {
+    pub async fn new(window: &winit::window::Window) -> Self {
+        let size = window.inner_size();
+        let instance = wgpu::Instance::new(BackendBit::PRIMARY);
+        let surface = unsafe { instance.create_surface(window) };
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .unwrap();
 
-    pub async fn new() -> Self {
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    label: Some("Device and queue."),
+                    features: wgpu::Features::empty(),
+                    limits: wgpu::Limits::default(),
+                },
+                None,
+            )
+            .await
+            .unwrap();
 
+        let sc_desc = wgpu::SwapChainDescriptor{
+            usage: wgpu::TextureUsage::RENDER_ATTACHMENT,
+            format: adapter.get_swap_chain_preferred_format(&surface).unwrap(),
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Fifo
+        };
+        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
+
+        let vert_shader = device.create_shader_module(&wgpu::include_spirv!("shaders/shader.frag.spv"));
+        let frag_shader = device.create_shader_module(&wgpu::include_spirv!("shaders/shader.frag.spv"));
+
+        let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor{
+            label: Some("render_pipeline_layout"),
+            bind_group_layouts: &[],
+            push_constant_ranges: &[]
+        });
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor{
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &vert_shader,
+                entry_point: "main",
+                buffers: &[]
+            },
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                clamp_depth: false,
+                polygon_mode: wgpu::PolygonMode::Fill,
+                conservative: false
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false
+            },
+            fragment: Some(wgpu::FragmentState{
+                module: &frag_shader,
+                entry_point: "main",
+                targets: &[
+                    wgpu::ColorTargetState{
+                        format: sc_desc.format,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrite::ALL
+                    }
+                ]
+            })
+        });
+
+        State {
+            surface,
+            device,
+            queue,
+            sc_desc,
+            swap_chain,
+            size,
+            render_pipeline
+        }
     }
 
-    pub fn update() {
+    pub fn update() {}
 
+    pub fn render() -> Result<(), wgpu::SwapChainError> {
+        Ok(())
     }
 
-    pub fn render() -> Result<wgpu::> {
-
+    pub fn input() -> bool {
+        false
     }
 
-    pub fn input() -> Self {
-
-    }
-
+    pub fn resize(&mut self, size: winit::dpi::PhysicalSize<u32>) {}
 }
