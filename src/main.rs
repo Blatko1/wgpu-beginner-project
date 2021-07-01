0d6f839534c9629a37b3a6ddc5024c658082792b
Leon <kings.of.funny.gemplays@gmail.com>
2021-07-01 17:37:13 +0200

    Created a window and started programming main app state and it's functions.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+use winit::{
+    dpi,
+    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+mod main_state;
+
+use main_state::State;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    let state = State::new();
+
+    window.set_title("wgpu graphics");
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Poll;
+        match event {
+            Event::WindowEvent { window_id, event } if window_id == window.id() => match event {
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                WindowEvent::Resized(new_size) => {
+                    state.resize(new_size);
+                }
+                WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                    state.resize(*new_inner_size);
+                }
+                _ => (),
+            },
+            Event::MainEventsCleared => {
+                window.request_redraw();
+            }
+            Event::RedrawRequested(_) => {
+                state.update();
+
+                match state.render() {
+                    Ok(_) => {}
+                    // Recreate the swap_chain if lost
+                    Err(wgpu::SwapChainError::Lost) => state.resize(state.size),
+                    // The system is out of memory, we should probably quit
+                    Err(wgpu::SwapChainError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                    // All other errors (Outdated, Timeout) should be resolved by the next frame
+                    Err(e) => eprintln!("{:?}", e),
+                }
+            }
+            _ => (),
+        }
+    })
 }
---
+++ src/main_state.rs
@@ -0,0 +1,23 @@
+pub struct State{
+
+}
+
+impl State{
+
+    pub async fn new() -> Self {
+
+    }
+
+    pub fn update() {
+
+    }
+
+    pub fn render() -> Result<wgpu::> {
+
+    }
+
+    pub fn input() -> Self {
+
+    }
+
+}
